3aa509f36a8f2515a00806d897e97b758b1fe1e8
sistemacodigolucrativo <sistemacodigolucrativo@gmail.com>
2026-08-27 19:52:28 -0300

    refactor: remover rota de comunicações administrativas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,7 +29,6 @@ import AdminSupport from "./pages/AdminSupport";
 import ApplicationConfirmation from "./pages/ApplicationConfirmation";
 import ApplicationPayment from "./pages/ApplicationPayment";
 import ApplicationTracking from "./pages/ApplicationTracking";
-import AdminCommunications from "./pages/AdminCommunications";
 import MemberReferrals from "./pages/MemberReferrals";
 import AdminReferrals from "./pages/AdminReferrals";
 import MemberCredentials from "./pages/MemberCredentials";
@@ -133,7 +132,6 @@ function AppRoutes() {
     <Route path="/admin/suporte" component={AdminSupport} />
     <Route path="/admin/auditoria" component={AdminAudit} />
     <Route path="/admin/pedidos" component={AdminOperations} />
-    <Route path="/admin/comunicacoes" component={AdminCommunications} />
     <Route path="/admin/financeiro" component={AdminOperations} />
     <Route path="/admin/relatos" component={AdminTestimonials} />
     <Route path="/admin/academia" component={AdminCourses} />
