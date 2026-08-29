8855022e933c82c637809b422954caaa21cba0bd
sistemacodigolucrativo <sistemacodigolucrativo@gmail.com>
2026-08-29 18:18:19 -0300

    feat: montar FAQ dentro de Configurar Seções

---
+++ client/src/pages/AdminSalesSectionsPage.tsx
@@ -0,0 +1,9 @@
+import AdminFaqManager from "@/components/AdminFaqManager";
+import AdminSalesImages from "@/pages/AdminSalesImages";
+
+export default function AdminSalesSectionsPage() {
+  return <>
+    <AdminSalesImages />
+    <AdminFaqManager />
+  </>;
+}
